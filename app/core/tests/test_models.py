aa7a44b5618cfef1c51df0efe1fad5665d005b42
Qingyuan Ma <qm28@georgetown.edu>
2020-08-24 22:54:29 -0400

    add the normalized email of lower case

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,3 +15,11 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@LONDONAPPDEV.COM'
+        user = get_user_model().objects.create_user(email, "123")
+         
+        self.assertEqual(user.email, email.lower())
+        
